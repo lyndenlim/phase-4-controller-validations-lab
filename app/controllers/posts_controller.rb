f0372de9d3ff0289b6c2a3f542092755a081d6a8
Lynden Lim <lyndenlim15@gmail.com>
2022-03-14 14:30:37 -0400

    commit tree 85e039dae6bd75a84182b3d7bd21d8c043a5b629

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,27 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def post_params
+    params.permit(:category, :content, :title)
+  end
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
   private
 
-  def post_params
-    params.permit(:category, :content, :title)
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
-
 end
